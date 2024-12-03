63f3290b3aafd05f3943b7627c5d0f832691bb27
xueldx <13790756845@163.com>
2024-12-03 21:00:40 +0800

    feat: 加载问卷数据的hook

--- questionnaire-fe/src/hooks/useLoadQuestionData.ts
+++ questionnaire-fe/src/hooks/useLoadQuestionData.ts
@@ -1,22 +1,13 @@
 import apis from '@/apis'
-import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useRequest } from 'ahooks'
 
 function useLoadQuestionData() {
   const { id = '' } = useParams()
-  // const [loading, setLoading] = useState(true)
-  // const [questionData, setQuestionData] = useState({})
   const getQuestionData = async () => {
     const data = await apis.getQuestionById(id)
     return data
-    // setQuestionData(data)
-    // setLoading(false)
   }
-
-  // useEffect(() => {
-  //   getQuestionData()
-  // }, [])
 
   const { loading, data, error } = useRequest(getQuestionData)
   return { loading, data, error }
