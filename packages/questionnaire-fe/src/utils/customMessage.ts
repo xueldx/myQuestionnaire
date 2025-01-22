1d5cf071bc8c96dfa566a6ccbb49c80e8f2a0e0f
xueldx <13790756845@163.com>
2025-01-23 01:09:40 +0800

    feat: FE - 调整前端首页样式与布局

---
+++ packages/questionnaire-fe/src/utils/customMessage.ts
@@ -0,0 +1,49 @@
+import { JointContent } from 'antd/es/message/interface'
+
+export const MESSAGE_EVENT_NAME = 'custom-antd-message'
+
+export enum MESSAGE_TYPES {
+  SUCCESS = 'success',
+  ERROR = 'error',
+  INFO = 'info',
+  WARNING = 'warning',
+  LOADING = 'loading'
+}
+
+const dispatch = (
+  type: MESSAGE_TYPES,
+  content: JointContent,
+  duration?: number | VoidFunction,
+  onClose?: VoidFunction
+) => {
+  window.dispatchEvent(
+    new CustomEvent(MESSAGE_EVENT_NAME, {
+      detail: {
+        params: {
+          content,
+          duration,
+          onClose
+        },
+        type: type
+      }
+    })
+  )
+}
+
+export const message = {
+  success(content: JointContent, duration?: number | VoidFunction, onClose?: VoidFunction) {
+    dispatch(MESSAGE_TYPES.SUCCESS, content, duration, onClose)
+  },
+  error(content: JointContent, duration?: number | VoidFunction, onClose?: VoidFunction) {
+    dispatch(MESSAGE_TYPES.ERROR, content, duration, onClose)
+  },
+  info(content: JointContent, duration?: number | VoidFunction, onClose?: VoidFunction) {
+    dispatch(MESSAGE_TYPES.INFO, content, duration, onClose)
+  },
+  warning(content: JointContent, duration?: number | VoidFunction, onClose?: VoidFunction) {
+    dispatch(MESSAGE_TYPES.WARNING, content, duration, onClose)
+  },
+  loading(content: JointContent, duration?: number | VoidFunction, onClose?: VoidFunction) {
+    dispatch(MESSAGE_TYPES.LOADING, content, duration, onClose)
+  }
+}
