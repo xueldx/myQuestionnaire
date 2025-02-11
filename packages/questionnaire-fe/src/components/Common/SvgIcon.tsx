5747b733c4a94b667dd731afaa6a0aa3eba2f83f
xueldx <13790756845@163.com>
2025-02-11 14:40:32 +0800

    feat: FE - 鉴权页面添加背景图与缓入动画

--- packages/questionnaire-fe/src/components/Common/SvgIcon.tsx
+++ packages/questionnaire-fe/src/components/Common/SvgIcon.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
-const SvgIcon: React.FC<{ name: string; prefix?: string; color?: string }> = ({
+const SvgIcon: React.FC<{ name: string; prefix?: string; color?: string; size?: string }> = ({
   name,
   prefix = 'icon',
   color = '#333',
+  size = '1em',
   ...props
 }) => {
   const symbolId = `#${prefix}-${name}`
 
   return (
-    <svg {...props} aria-hidden="true">
+    <svg {...props} width={size} height={size} aria-hidden="true">
       <use href={symbolId} fill={color} />
     </svg>
   )
