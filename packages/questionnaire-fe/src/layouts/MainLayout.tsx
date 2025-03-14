3a0f581bf5c76e81e9ab10b4f7ec8c620c487ed7
xueldx <13790756845@163.com>
2025-03-14 14:20:36 +0800

    feat: FE - 页脚内容优化

--- packages/questionnaire-fe/src/layouts/MainLayout.tsx
+++ packages/questionnaire-fe/src/layouts/MainLayout.tsx
@@ -31,8 +31,10 @@ const MainLayout: React.FC = () => {
         <Outlet />
       </main>
       <footer className="custom-footer">
-        小木问卷 &copy;2024 - present. Created by{' '}
-        <Link to="https://indulgeback.netlify.app/">IndulgeBack</Link> Version: {__APP_VERSION__}
+        <div>
+          小木问卷 &copy;2024 - present. Created by{' '}
+          <Link to="https://indulgeback.netlify.app/">IndulgeBack</Link> Version: {__APP_VERSION__}
+        </div>
       </footer>
       <Spin indicator={<CustomSpin />} spinning={screenSpinning} fullscreen />
     </div>
