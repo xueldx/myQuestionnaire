4f0c0a714607063c00461240816460ff3b5110b3
xueldx <13790756845@163.com>
2025-04-10 14:35:06 +0800

    feat: CLIENT - 新增客户端

---
+++ packages/questionnaire-client/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { title } from "@/components/primitives";
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className={title()}>About</h1>
+    </div>
+  );
+}
