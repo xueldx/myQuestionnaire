7a4c40625e6a828a6459d8d8279592b4385f56b2
xueldx <13790756845@163.com>
2025-04-23 06:56:06 +0800

    chore: 代码仓库整理

--- packages/questionnaire-client/app/answer/page.tsx
+++ packages/questionnaire-client/app/answer/page.tsx
@@ -1,7 +1,7 @@
 import QuestionActions from "@/components/question-ui/questionActions";
 import QuestionWrapper from "@/components/question-ui/questionWrapper";
-import QuestionCheckbox from "@/components/questions/questionCheckbox";
-import QuestionRadio from "@/components/questions/questionRadio";
+import QuestionCheckbox from "@/components/question-type/questionCheckbox";
+import QuestionRadio from "@/components/question-type/questionRadio";
 import React from "react";
 
 const AnswerPage = () => {
